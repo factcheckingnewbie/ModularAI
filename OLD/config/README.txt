85f7cde17218acfe28349377f8a0d536ad8cd4cd
ptr <200551791+factcheckingnewbie@users.noreply.github.com>
2025-05-28 19:26:15 +0200

    deleted:    OLD/config/README.txt 	deleted:    OLD/config/__init__.py 	deleted:    OLD/config/interface_modules.json 	deleted:    OLD/config/model_modules.json 	deleted:    OLD/core/__init__.py 	deleted:    OLD/core/module_controller.py 	deleted:    OLD/core/module_controller_sav.py

--- OLD/config/README.txt
+++
@@ -1,1 +0,0 @@
-Overriden "structured_output in model_modules.json true, instead of alse to get module_controller to accept the module
--- OLD/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-./core
-"""
-"""
-Core components for the ModdularAI framework.
-
-This package contains essential components like the module controller
-that mediates between interfaces and AI models.
-"""
